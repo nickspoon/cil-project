3f3a75b04ef51f22a758d78af50d235799f3b003
Pedro <pedrini@public-docking-hg-2009.ethz.ch>
2014-06-12 10:26:52 +0200

    Adding the evaluate compression thing
    result not good!

---
+++ nmf/Compress.m
@@ -0,0 +1,17 @@
+function I_comp = Compress(I)
+    d = 20; % patch size
+    k = 1; % dimensions to retain
+%     c = 16; % number of k-means clusters
+    
+	[U, Z] = NoNMF(I, d, k);
+% 	[mu, lambda, U] = PCAanalyse(X);
+	
+%     Xzm = X - repmat(mu, size(X, 1), 1);
+%     Zzm = U' * Xzm';
+%     [mus, Z, msd] = Kmeans(Zzm(1:k,:)', c);
+%     
+%     sz = size(I);
+%     colours = size(I, 3);
+%     PCAdata = struct('mean', mu, 'eigen', U(:, 1:k), 'blocks', blocks, 'patch', d);
+%     Kmdata = struct('means', mus, 'data', uint8(Z));
+    I_comp = struct('U', U, 'Z', Z, 'd', d, 'k', k);
