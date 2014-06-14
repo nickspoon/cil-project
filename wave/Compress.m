949a656923085347a0bcb9beba7919d2014e9e18
Pedro <pedrini@public-docking-cx-2318.ethz.ch>
2014-06-14 15:34:21 +0200

    Work Fine
    So nice know it works perfectly but still not enough compression

--- wave/Compress.m
+++ wave/Compress.m
@@ -5,23 +5,42 @@ function I_comp = Compress(I)
     datl=size(I);
     I2=I;
     if isequal(size(datl,2),3)
-        [I2,sz]=color(I2,d);
-        
+        [IR,IG,IB,sz]=color(I2,d);
+        for i=1:size(IR,2)
+            perro=vec2mat(IG(:,i)',d);
+            perro2=vec2mat(IG(:,i)',d);
+            perro3=vec2mat(IB(:,i)',d);
+    %         'perro';
+    %         size(I);
+            [HAH,H]=Harr(perro);
+            [HAH2,H2]=Harr(perro2);
+            [HAH3,H3]=Harr(perro3);
+    %         size(H);
+    %         size(HAH);
+            HAH=H'\HAH/H;
+            HAH2=H2'\HAH2/H2;
+            HAH3=H3'\HAH3/H3;
+            IR(:,i)=reshape(HAH,1,d*d);
+            IG(:,i)=reshape(HAH2,1,d*d);
+            IB(:,i)=reshape(HAH3,1,d*d);
+        end
+        I2=cat(2,IR,IG,IB);
     elseif isequal(size(datl,2),2)
         [I2,sz]=nocolor(I2,d);
+        for i=1:size(I2,2)
+            perro=vec2mat(I2(:,i)',d);
+    %         'perro';
+    %         size(I);
+            [HAH,H]=Harr(perro);
+    %         size(H);
+    %         size(HAH);
+            HAH=H'\HAH/H;
+            I2(:,i)=reshape(HAH,1,d*d);
+        end
     end
 %  	X = extract(I, d);
 
-    for i=1:size(I2,2)
-        perro=vec2mat(I2(:,i)',d);
-%         'perro';
-%         size(I);
-        [HAH,H]=Harr(perro);
-%         size(H);
-%         size(HAH);
-        HAH=H'\HAH/H;
-        I2(:,i)=reshape(HAH,1,d*d);
-    end
+    
 %     I2=I2';
 %     size(I)
     I_comp = struct('d', d, 'X', I2,'size',size(I),'size2',sz,'colours',    size(I2,2));
--- wave/color.m
+++ wave/color.m
@@ -1,4 +1,4 @@
-function [x1,sz]=color(i2,d2)
+function [xR,xG,xB,sz]=color(i2,d2)
 [x,y,z]=size(i2);
 i2_red=i2(:,:,1);
 i2_green=i2(:,:,2);
@@ -19,17 +19,25 @@ if not(isequal(mod(y,d2),0));
    end
 end
 sz=size(i2_red);
-x3=cat(2,i2_red,i2_green,i2_blue);
-[x11,y1]=size(x3);
+% x3=cat(2,i2_red,i2_green,i2_blue);
+[x11,y1]=size(i2_red);
 xR=[];
+xG=[];
+xB=[];
 for i=1:d2:x11
     for j=1:d2:y1
-        x4=x3(i:i-1+d2,j:j-1+d2)';
+        x4=i2_red(i:i-1+d2,j:j-1+d2);
         x4=x4(:);
         xR=cat(2,xR,x4);
   
+        x5=i2_green(i:i-1+d2,j:j-1+d2);
+        x5=x5(:);
+        xG=cat(2,xG,x5);
+        
+        x6=i2_blue(i:i-1+d2,j:j-1+d2);
+        x6=x6(:);
+        xB=cat(2,xB,x6);
     end
 end
-x1=xR;
 end
 
